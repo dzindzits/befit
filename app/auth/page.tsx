b4e21c3ad3223ec9b388f758cc809110a48bd659
Kristers Dzintars <kristers.dzintars@pearlgroup.no>
2023-11-09 04:59:38 +0200

    Added password type

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -30,7 +30,7 @@ export default function Auth() {
             <p className="text-md text-white/50">Login with your username and password</p>
             <div className="flex flex-col gap-1">
                 <input name="username" value={form.username} onChange={onInput()} placeholder="Username" className="p-2 bg-transparent border border-yellow-400 text-white rounded-lg" />
-                <input name="password" value={form.password} onChange={onInput()} placeholder="Password" className="p-2 bg-transparent border border-yellow-400 text-white rounded-lg" />
+                <input name="password" type="password" value={form.password} onChange={onInput()} placeholder="Password" className="p-2 bg-transparent border border-yellow-400 text-white rounded-lg" />
             </div>
             <button onClick={onSubmit()} className="rounded-lg bg-yellow-400/50 hover:bg-yellow-400/75 p-2">Log in</button>
         </form>
